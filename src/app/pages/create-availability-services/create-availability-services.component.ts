cf3c1d498f948c9c435c1c8d7304c605c0ba5709
Mario Beltran <megamariossj21@gmail.com>
2022-03-01 14:21:09 -0500

    Se añaden nuevos formularios - Mario

---
+++ src/app/pages/create-availability-services/create-availability-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-availability-services',
+  templateUrl: './create-availability-services.component.html',
+  styleUrls: ['./create-availability-services.component.css']
+})
+export class CreateAvailabilityServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
