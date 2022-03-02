e4d8ac2f35c556eabb6411160dd9cfdbae236715
Mario Beltran <megamariossj21@gmail.com>
2022-03-02 16:39:06 -0500

    Se añaden nuevas funcionalidades form - Mario

--- src/app/pages/create-availability-services/create-availability-services.component.ts
+++ src/app/pages/create-availability-services/create-availability-services.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GeneralService } from 'src/app/services/general.service'
 
 @Component({
   selector: 'app-create-availability-services',
@@ -6,10 +8,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-availability-services.component.css']
 })
 export class CreateAvailabilityServicesComponent implements OnInit {
+  listPeriod: any = [];
+  formRegisterAvailabilityService: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _generalService: GeneralService,
+  ) { 
+    this.formRegisterAvailabilityService = this.createFormRegisterAvailabilityService();
+  }
 
   ngOnInit(): void {
+    this.loadPeriod();
   }
+  createFormRegisterAvailabilityService() {
+    return new FormGroup({
+      period: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+      ]),
+      standard_value: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(35),
+        Validators.minLength(5),
+      ]),
+      critical_value: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(15),
+        Validators.minLength(5),
+      ]),
+      total_value: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(15),
+        Validators.minLength(5),
+      ]),
+      hour_failed: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(35),
+        Validators.minLength(5),
+      ])
+    });
+  }
+  loadPeriod() {
+    this._generalService.generateConsult('periods').subscribe(
+      (res) => {
+        this.listPeriod = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  generateRequest(data: any) {
+    const objRequest = {
+
+      "period_id": data['period'],
+      "type_work_id": "4",
+      "standard_value": data['standard_value'],
+      "critical_value": data['critical_value'],
+      "total_hours": data['total_value'],
+      "failures_hours": data['hour_failed']
+    };
+  
+    this.formRegisterAvailabilityService.reset();
+    this._generalService.generateCreate('employee_periods', objRequest).subscribe(
+      (res) => {
+        console.log(res)
+      }, (error) => {
+        console.log(error);
+      }
+    );
+  }
+
 
 }
--- src/app/pages/create-money/create-money.component.ts
+++ src/app/pages/create-money/create-money.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GeneralService } from 'src/app/services/general.service'
 
 @Component({
   selector: 'app-create-money',
@@ -6,10 +8,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-money.component.css']
 })
 export class CreateMoneyComponent implements OnInit {
+  listPeriod: any = [];
+  listEmployee: any = [];
+  formRegisterMoney: FormGroup;
 
-  constructor() { }
-
+  constructor(
+    private _generalService: GeneralService,
+  ) {
+    this.formRegisterMoney = this.createFormRegisterMoney();
+  }
   ngOnInit(): void {
+    this.loadPeriod();
+    this.loadEmployee();
   }
 
+  createFormRegisterMoney() {
+    return new FormGroup({
+      period: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+      ]),
+      employee: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+      ]),
+      activity_compliance: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(15),
+        Validators.minLength(5),
+      ]),
+      help_desk: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(35),
+        Validators.minLength(5),
+      ]),
+      availability_service: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(15),
+        Validators.minLength(5),
+      ]),
+      levels_service: new FormControl({ value: null, disabled: false }, [
+        Validators.required,
+        Validators.maxLength(35),
+        Validators.minLength(5),
+      ])
+    });
+  }
+  loadPeriod() {
+    this._generalService.generateConsult('periods').subscribe(
+      (res) => {
+        this.listPeriod = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadEmployee() {
+    this._generalService.generateConsult('employees').subscribe(
+      (res) => {
+        this.listEmployee = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  generateRequest(data: any) {
+    const objRequest = {
+      "employee_id": data['employee'],
+      "period_id": data['period'],
+      "type_work_id": "3",
+      "cumplimiento_actividades": data['activity_compliance'],
+      "help_table": data['help_desk'],
+      "provision_services": data['availability_service'],
+      "services_levels": data['levels_service']
+    };
+  
+    this.formRegisterMoney.reset();
+    this._generalService.generateCreate('employee_periods', objRequest).subscribe(
+      (res) => {
+        console.log(res)
+      }, (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
